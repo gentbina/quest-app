ac83c4568543c84d8f7a65e46a8992700e26a91b
gentbina <gbina@GentsMacStudio.localdomain>
2024-05-24 12:30:52 -0400

    Clean up main_tf

--- main.tf
+++ main.tf
@@ -2,33 +2,33 @@ module "quest-app" {
   source = "./module"
 
   #ECS
-  ecs_cluster_name       = "quest-app-cluster"
-  ecs_task_family        = "quest-app-task"
-  ecs_task_cpu           = "1024"
-  ecs_task_memory        = "2048"
-  ecs_execution_role_arn = "arn:aws:iam::191600387477:role/ecsTaskExecutionRole"
-  container_name         = "quest-app"
-  container_image        = "191600387477.dkr.ecr.us-east-1.amazonaws.com/quest-app:latest"
-  container_port         = "3000"
-  host_port              = "3000"
-  env_variable           = "SECRET_WORD"
-  env_variable_value     = "TwelveFactor"
-  ecs_service_name       = "quest-app-service"
-  desired_count          = "2"
+  ecs_cluster_name       = "random-cluster-name"
+  ecs_task_family        = "random-task-family"
+  ecs_task_cpu           = "512"
+  ecs_task_memory        = "1024"
+  ecs_execution_role_arn = "arn:aws:iam::123456789012:role/randomTaskExecutionRole"
+  container_name         = "random-container"
+  container_image        = "123456789012.dkr.ecr.us-east-1.amazonaws.com/random-container:latest"
+  container_port         = "8080"
+  host_port              = "8080"
+  env_variable           = "SECRET_KEY"
+  env_variable_value     = "RandomValue123"
+  ecs_service_name       = "random-service"
+  desired_count          = "3"
   assing_public_ip       = true
 
   #Load Balancer
-  lb_name              = "quest-app-lb"
-  lb_target_group_name = "quest-app-tg"
-  lb_target_group_port = "3000"
-  lb_target_type       = "ip"
+  lb_name              = "random-lb"
+  lb_target_group_name = "random-tg"
+  lb_target_group_port = "8080"
+  lb_target_type       = "instance"
 
   #Security Groups
-  ecs_sg_name = "ecs_security_group"
-  alb_sg_name = "alb_security_group"
+  ecs_sg_name = "random_ecs_security_group"
+  alb_sg_name = "random_alb_security_group"
 
   ecs_ingress_ports = [
-    { from_port = 3000, to_port = 3000, protocol = "tcp", cidr = "0.0.0.0/0" }
+    { from_port = 8080, to_port = 8080, protocol = "tcp", cidr = "0.0.0.0/0" }
   ]
 
   alb_ingress_ports = [
@@ -41,54 +41,54 @@ module "quest-app" {
   ]
 
   #IAM
-  role_name = "ecsTaskExecutionRole"
+  role_name = "randomTaskExecutionRole"
   managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+    "arn:aws:iam::aws:policy/service-role/RandomECSTaskExecutionRolePolicy"
   ]
 
   #Route53 and ACM
-  domain_name             = "test.gentbina.com"
-  zone_id                 = "ZFAKQT1VB7ZSE"
+  domain_name             = "example.com"
+  zone_id                 = "Z1234567890ABC"
   validation_method       = "DNS"
   record_type             = "A"
-  evaluate_target_health  = true
-  r53_cert_validation_ttl = 60
+  evaluate_target_health  = false
+  r53_cert_validation_ttl = 300
 
   #Providers Block
-  region           = "us-east-1"
-  account_id       = 191600387477
-  deploy_role_name = "terraform-deploy"
+  region           = "us-west-2"
+  account_id       = 123456789012
+  deploy_role_name = "random-deploy-role"
 
   #VPC
-  vpc_cidr = "10.0.0.0/16"
+  vpc_cidr = "192.168.0.0/16"
 
   subnet_cidrs = [
-    "10.0.0.0/24",
-    "10.0.1.0/24"
+    "192.168.1.0/24",
+    "192.168.2.0/24"
   ]
 
   availability_zones = [
-    "us-east-1a",
-    "us-east-1b"
+    "us-west-2a",
+    "us-west-2b"
   ]
 }
 
 output "ecs_cluster_id" {
-  description = "The ID of the ECS cluster from the qust-app module"
+  description = "The ID of the ECS cluster from the random module"
   value       = module.quest-app.ecs_cluster_id
 }
 
 output "load_balancer_dns" {
-  description = "The DNS name of the Load Balancer from the qust-app module"
+  description = "The DNS name of the Load Balancer from the random module"
   value       = module.quest-app.load_balancer_dns
 }
 
 output "route53_record_name" {
-  description = "The name of the Route 53 record from the qust-app module"
+  description = "The name of the Route 53 record from the random module"
   value       = module.quest-app.route53_record_name
 }
 
 output "certificate_arn" {
-  description = "The ARN of the ACM certificate from the qust-app module"
+  description = "The ARN of the ACM certificate from the random module"
   value       = module.quest-app.certificate_arn
 }
